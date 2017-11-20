9703d565f85e547fe82da8c38505c9f2f51d1e46
Damien Dussauge <damien.dussauge@gmail.com>
2017-11-20 01:04:13 +0100

    Population generation, generation by rank, fitness, crossing and mutating

---
+++ js/algo.js
@@ -0,0 +1,232 @@
+var PROBA_CROSSING = 0.8725;	// Probability of crossing parents for generating children
+var PROBA_MUTATE = 0.05	// Probability of mutating the newly generated chromosome
+var POPULATION_SIZE = 20;
+var sumOfAllFitness = 0;	// Updated by evaluateEveryFitness()
+var wheel = [];
+
+//	Randomly generates a population of POPULATION_SIZE chromosomes
+function generatePopulation() {
+	// Generate POPULATION_SIZE chromosomes
+	for(var i = 0 ; i < POPULATION_SIZE ; i++) {
+		var chromosome = [];
+		for(var j = 0 ; j < nodes.length ; j++) {	//	Generate a chromosome containing one time every node's id
+			chromosome.push(nodes[j].id);
+		}
+		shuffle(chromosome);
+		population.push(chromosome);
+	}
+}
+
+//	Generates a new population using the wheel method
+function wheelGeneration() {
+	
+	/*var fitnessList = evaluateEveryFitness();
+	var fitnessPercentages = {};	// fitnessPercentages[1180.8] = 0.1857, fitnessPercentages[5609] = 0.03...
+	for(var i = 0 ; i < Object.keys(fitnessList).length ; i++) {
+		fitnessPercentages[fitnessList[i]] = fitnessList[i] / sumOfAllFitness;
+	}
+
+	var tempArray = fitnessPercentages;	// Copy of fitnessPercentages
+
+
+
+	// DESC sorted fitnessPercentages.values
+	var fitnessPercentagesSorted = Object.keys(fitnessPercentages).sort();	// DESC : sort(function(a, b){return b-a})
+	var realPercentages = [];
+
+	var startingPercentage = 0;
+	for(var i = 0 ; i < fitnessPercentagesSorted.length ; i++) {
+		fitnessPercentagesSorted[i];
+	}
+
+	for(var i = 0 ; i < Object.keys(fitnessPercentages).length ; i++) {
+		
+	}
+	// Wheel selection
+	var parentsSelectionCompleted = false, parent1 = null, parent2 = null;
+	while(!parentsSelectionCompleted) {
+		console.log("while");
+		for(var i = 0 ; i < population.length ; i++) {
+			console.log(i);
+			if(fitnessPercentages[getFitness(population[i])] > Math.random()) {	// Select the chromosome
+				if(parent1 == null) 
+					parent1 = population[i];
+				else if(parent2 == null) {
+					parent2 = population[i];
+					parentsSelectionCompleted = true;
+				}
+				else {	// Both are set
+					parentsSelectionCompleted = true;
+					break;
+				}	
+
+			}
+		}
+	}*/
+
+}
+
+
+//	Generates a new population using the rank method
+function rankGeneration() {
+	var fitnessList = evaluateEveryFitness();
+	var sortedFitness = Object.keys(fitnessList).sort(function(a, b) {	// Sort DESC
+    	return fitnessList[b] - fitnessList[a];
+	});
+
+	var sortedFitnessList = {};
+	var rank = 1;
+	for(var i = 0 ; i < sortedFitness.length ; i++) {
+		if(sortedFitnessList[fitnessList[sortedFitness[i]]] == null)
+			sortedFitnessList[fitnessList[sortedFitness[i]]] = rank++;
+	}
+
+	newPopulation = [];
+	while(newPopulation.length < population.length) {
+
+		var parentsSelectionCompleted = false, parent1 = null, parent2 = null;
+	
+		for(var i = 0 ; i < population.length ; i++) {
+			if(sortedFitnessList[getFitness(population[i])] >= randomInt(1, rank-1)) {	// Select the chromosome
+				if(parent1 == null) 
+					parent1 = population[i];
+				else if(parent2 == null) {
+					parent2 = population[i];
+					parentsSelectionCompleted = true;
+				}
+				else {	// Both are set
+					parentsSelectionCompleted = true;
+					break;
+				}	
+
+			}
+		}
+
+		if(!parentsSelectionCompleted)
+			console.log("ALERT : PARENTS NOT SELECTED");
+
+
+		newPopulation.push(crossing(parent1, parent2));
+		newPopulation.push(crossing(parent2, parent1));
+
+	}
+
+	for(var i = 0 ; i < newPopulation.length ; i++) {
+		mutate(newPopulation[i]);
+	}
+
+	population = newPopulation.splice(0);
+}
+
+function crossing(chromosome1, chromosome2) {
+	//console.log("--------CROSSING");
+	//console.log(chromosome1, chromosome2);
+	var currentChromosome = chromosome1.slice(0), otherChromosome = chromosome2.slice(0);
+	var currentNode;
+	var solution = [], availableNodes = chromosome1.slice(0);
+	var stayingValue, crossingValue;
+
+	solution.push(currentChromosome[0]);
+	if(availableNodes.length > 0)
+		availableNodes.splice(availableNodes.indexOf(currentChromosome[0]), 1);
+
+
+	for(var i = 0 ; i < chromosome1.length - 1 ; i++) {
+		currentNode = currentChromosome[i];
+
+		
+		if(currentChromosome[currentChromosome.length-1] != currentNode)
+			stayingValue = currentChromosome[i+1];
+		else
+			stayingValue = currentChromosome[0];
+
+		if(otherChromosome[otherChromosome.length-1] != currentNode)	// currentNode not the last value in otherChromosome
+			crossingValue = otherChromosome[otherChromosome.indexOf(currentNode) + 1];
+		else
+			crossingValue = otherChromosome[0];
+
+		
+		//console.log(currentNode, stayingValue, crossingValue);
+		if(Math.random() < PROBA_CROSSING) {	// Crossing
+
+			currentNode = crossingValue;
+			currentChromosome = (currentChromosome == chromosome1 ? chromosome2:chromosome1);
+		} else {	// No crossing
+			currentNode = stayingValue;
+		}
+
+		//console.log(currentNode, availableNodes);
+
+		if(solution.includes(currentNode)) {
+			var n = (availableNodes.length == 1 ? availableNodes[0]:availableNodes[randomInt(0, availableNodes.length - 1)])
+			solution.push(n);
+			if(availableNodes.length > 0)
+				availableNodes.splice(availableNodes.indexOf(n), 1);
+		}
+		else {
+			solution.push(currentNode);
+			if(availableNodes.length > 0)
+				availableNodes.splice(availableNodes.indexOf(currentNode), 1);
+		}
+		//console.log(solution);
+	}
+
+	return solution;
+}	
+
+function mutate(chromosome) {
+	if(Math.random() <= PROBA_MUTATE) {
+		console.log("MUTATE");
+		console.log(chromosome);
+
+		var availableNodes = chromosome.slice(0);
+
+		var rand = randomInt(0, chromosome.length - 1);	// Index of value we have to mutate
+		var toMutate = chromosome[rand];	// Value we have to mutate
+
+		availableNodes.splice(availableNodes.indexOf(toMutate), 1);	// Can't mutate to its own value
+		//console.log(availableNodes);
+		var newValue = availableNodes[randomInt(0, availableNodes.length - 1)];	// New value of mutating octal
+		//console.log(chromosome[rand], newValue, chromosome.indexOf(newValue), toMutate);
+		chromosome[chromosome.indexOf(newValue)] = toMutate;
+		chromosome[rand] = newValue;
+		console.log(chromosome);
+	}
+}
+
+
+//	Returns the fitness of the chromosome
+function getFitness(chromosome) {
+	var pathDistance = 0;
+	for(var i = 0 ; i < chromosome.length - 1 ; i++) {
+		pathDistance += distance(nodes[chromosome[i] - 1], nodes[chromosome[i+1] - 1]);
+	}
+	pathDistance += distance(nodes[chromosome[0] - 1], nodes[chromosome[chromosome.length - 1] - 1]);
+	return pathDistance;
+}
+
+//	Evaluates the fitness of every chromosome in the population
+function evaluateEveryFitness() {
+	var fitnessList = {}, fitness = 0;
+	for(var i = 0 ; i < population.length ; i++) {
+		fitness = getFitness(population[i]);
+		fitnessList[i] = fitness;
+		sumOfAllFitness += fitness;
+	}
+	return fitnessList;
+}
+
+// Found on https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+}
+
+function randomInt(lower, upper) {
+	return Math.floor(Math.random() * upper) + 1;
+}
--- js/main.js
+++ js/main.js
@@ -5,7 +5,11 @@ var NUMBER_OF_NODES = 25;	// Determines how many nodes there will be in the init
 var CANVAS_X;
 var CANVAS_Y;
 var run = false;	// Determines wether the genetic algorithm is running or not
-var randomGenerating = false;	// Determines wether the nodes will be randomly generated or picked from a fixed data set
+var randomGenerating = true;	// Determines wether the nodes will be randomly generated or picked from a fixed data set
+var population = [];	// Population of NUMBER_OF_NODES chromosomes
+// var SELECTION_MODE = 1; 	// From 0 to 4 : wheel, rank, elitism, wheel+elitism, rank+elitism
+var tempPopulation = [];
+var newPopulation = [];
 
 $(document).ready(function() {
 	canvas = $("#canvas")[0];
@@ -14,15 +18,19 @@ $(document).ready(function() {
 	CANVAS_Y = canvas.height;
 
 	initNodes(randomGenerating);
-	drawing();
-
+	generatePopulation();
+	setInterval(function() {
+		drawing();
+		rankGeneration();
+	}, 10);
 
 });
 
+//	Inits nodes randomly or not according to rand
 function initNodes(rand) {
 	if(rand) {
 		for(var i = 0 ; i < NUMBER_OF_NODES ; i++) {
-			nodes.push(randomNode());
+			nodes.push(randomNode(i+1));
 			// TODO : check there isn't already another node with the same values
 		}
 	} else {
@@ -31,65 +39,86 @@ function initNodes(rand) {
 	
 }
 
+//	Inits the nodes with fixed values so that we can compare results with different parameters in the GA
 function initFixedNodes() {
-	nodes.push(new Node (388, 18));
-	nodes.push(new Node(212, 247));
-	nodes.push(new Node(111, 44));
-	nodes.push(new Node(10, 241));
-	nodes.push(new Node(376, 261));
-	nodes.push(new Node(356, 248));
-	nodes.push(new Node(195, 242));
-	nodes.push(new Node(140, 370));
-	nodes.push(new Node(84, 50));
-	nodes.push(new Node(306, 161));
-	nodes.push(new Node(4, 18));
-	nodes.push(new Node(192, 254));
-	nodes.push(new Node(221, 373));
-	nodes.push(new Node(334, 92));
-	nodes.push(new Node(232, 105));
-	nodes.push(new Node(217, 63));
-	nodes.push(new Node(375, 87));
-	nodes.push(new Node(340, 367));
-	nodes.push(new Node(133, 240));
-	nodes.push(new Node(43, 113));
-	nodes.push(new Node(236, 147));
-	nodes.push(new Node(229, 230));
-	nodes.push(new Node(189, 60));
-	nodes.push(new Node(42, 9));
-	nodes.push(new Node(289, 280));
+	nodes.push(new Node (388, 18,1));
+	nodes.push(new Node(212, 247,2));
+	nodes.push(new Node(111, 44,3));
+	nodes.push(new Node(10, 241,4));
+	nodes.push(new Node(376, 261,5));
+	nodes.push(new Node(356, 248,6));
+	nodes.push(new Node(195, 242,7));
+	nodes.push(new Node(140, 370,8));
+	nodes.push(new Node(84, 50,9));
+	nodes.push(new Node(306, 161,10));
+	nodes.push(new Node(4, 18,11));
+	nodes.push(new Node(192, 254,12));
+	nodes.push(new Node(221, 373,13));
+	nodes.push(new Node(334, 92,14));
+	nodes.push(new Node(232, 105,15));
+	nodes.push(new Node(217, 63,16));
+	nodes.push(new Node(375, 87,17));
+	nodes.push(new Node(340, 367,18));
+	nodes.push(new Node(133, 240,19));
+	nodes.push(new Node(43, 113,20));
+	nodes.push(new Node(236, 147,21));
+	nodes.push(new Node(229, 230,22));
+	nodes.push(new Node(189, 60,23));
+	nodes.push(new Node(42, 9,24));
+	nodes.push(new Node(289, 280,25));
 }
 
 
-function Node(x, y) {
+function Node(x, y, id) {
 	this.x = x;
 	this.y = y;
+	this.id = id;
 }
 
 // Return a new node instanciated with random values
-function randomNode() {
-	return new Node(parseInt(Math.random() * (CANVAS_X-10)), parseInt(Math.random() * (CANVAS_Y-10)));
+function randomNode(id) {
+	return new Node(parseInt(Math.random() * (CANVAS_X-10)), parseInt(Math.random() * (CANVAS_Y-10)), id);
 }
 
 // Returns distance between two nodes
 function distance(n1, n2) {
-	return Math.sqrt(n1.x - n2.x + n1.y - n2.y);
+	return Math.sqrt((n1.x - n2.x)*(n1.x - n2.x) + (n1.y - n2.y)*(n1.y - n2.y));
 }
 
+//	Draws nodes and then the path if the program is running
 function drawing() {
 	clear();	//	Clears in case something is already drawn on the canvas
 	if(nodes.length > 0) {
 		for(var i = 0 ; i < nodes.length ; i++) {
 			drawDot(nodes[i]);
 		}
+		drawPath(population[0]);	// Drawing first chromosome's path
 	}
 }
 
+//	Draw the dot representing the node
 function drawDot(node) {
 	context.fillStyle = "#000000";
 	context.beginPath();
 	context.arc(node.x, node.y, 3, 0, Math.PI * 2, true);
 	context.closePath();
 	context.fill();
+}
+
+//	Draws the path of the salesman represented by the chromosome
+function drawPath(chromosome) {
+	context.strokeStyle = '#00ff00';
+	context.lineWidth = 1;
+	context.beginPath();
+	context.moveTo(nodes[chromosome[0] - 1].x, nodes[chromosome[0] - 1].y);
+
+	for(var i = 1 ; i < chromosome.length ; i++)
+		context.lineTo(nodes[chromosome[i] - 1].x, nodes[chromosome[i] - 1].y);
+
+	context.lineTo(nodes[chromosome[0] - 1].x, nodes[chromosome[0] - 1].y);
+
+	context.stroke();
+	context.closePath();
 }
 
 // Clears everything in the canvas
